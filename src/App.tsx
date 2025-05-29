f3a38563ee9f9f34099e4255d7e3cb0df412df7e
Aidê <aidefpassos@gmail.com>
2025-05-28 23:03:36 -0300

    ajustes nos arquivos yml e app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           Hello World!
         </h1>
       </div>
-      <Link to="/one" className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300"      >
+      <Link to="/tech-studies/one" className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300"      >
         Go to Lesson 1
       </Link>
     </>
