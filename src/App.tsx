b313846dda616895d41b806d6eb0ddcaf3ceaec6
Aidê <aidefpassos@gmail.com>
2025-05-28 21:37:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-5xl font-bold text-blue-600">
+          Hello World!
+        </h1>
+      </div>
+      <Link to="/one" className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300"      >
+        Go to Lesson 1
+      </Link>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/One.tsx
@@ -0,0 +1,11 @@
+function Lesson1() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-5xl font-bold text-green-700">
+        Lição 1
+      </h1>
+    </div>
+  );
+}
+
+export default Lesson1;
